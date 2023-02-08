57231a71b5486728e6d536055aef29fb1d8bf3cb
Elijah Mooring <vehmloewff@gmail.com>
2023-02-07 21:49:30 -0800

    save progress

--- main.ts
+++ main.ts
@@ -9,5 +9,5 @@ import { renderWidget, requestWindow, Ui } from './deps.ts'
 export async function ui() {
 	const outletId = await requestWindow({ allowClose: true, allowMinimization: true, allowFullscreen: true, allowResize: true })
 
-	renderWidget(outletId, Ui.Center({ child: Ui.Text('Hello, World!') }))
+	renderWidget(outletId, Ui.Center({ child: Ui.Text('Hello, Elijah!') }))
 }
